fab76971a4ec904991d103badc4950bbaa3247a9
syedibrahimt <syedibrahim.developer@gmail.com>
2025-09-03 07:11:32 +0530

    assigment completion for string

---
+++ day1/string_assignment.py
@@ -0,0 +1,12 @@
+"""
+String Assignment. (This can be tricky so feel free to watch solution so we can do it together)
+
+- Ask the user how many days until their birthday
+
+- Using the print()function. Print an approx. number of weeks until their birthday
+
+- 1 week is = to 7 days.
+"""
+
+days = input("how many days until their birthday ?")
+print(f"Approximate weeks till birthday is {int(int(days) / 7)}")
