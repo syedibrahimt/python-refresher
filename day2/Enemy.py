8c101df3aa86ef97c6cdb085e447cc00ee595287
Syed Ibrahim <syedibrahim.developer@gmail.com>
2025-09-04 12:21:38 +0530

    Classes introduction

---
+++ day2/Enemy.py
@@ -0,0 +1,4 @@
+class Enemy:
+    type_of_enemy: str
+    health_points: int
+    attack_damage: int
--- day2/main.py
+++ day2/main.py
@@ -1,3 +1,10 @@
 from tax import calculate_income
+from Enemy import *
 
 calculate_income(100000, 10)
+
+enemy = Enemy()
+enemy.type_of_enemy = 'Fool'
+enemy.attack_damage = 1
+enemy.health_points = 100
+print(enemy)
