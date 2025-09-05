9366f4118af4c67b2089bc5b0bd3cca6f60af5e2
Syed Ibrahim <syedibrahim.developer@gmail.com>
2025-09-05 21:08:19 +0530

    Initializing enemy with default and parameterized constructor

--- day3/Enemy.py
+++ day3/Enemy.py
@@ -1,16 +1,18 @@
 class Enemy:
-    health_points: int
-    attack: int
-    type_of_enemy: str
+
+    def __init__(self, type_of_enemy, health_points = 100, attack_points = 1):
+        self.type_of_enemy = type_of_enemy
+        self.health_points = health_points
+        self.attack_points = attack_points
 
     def about_enemy(self):
-        print(f"The Enemy is of type {self.type_of_enemy} and he has {self.health_points} health points. He can do attack of {self.attack} point")
+        print(f"The Enemy is of type {self.type_of_enemy} and he has {self.health_points} health points. He can do attack of {self.attack_points} point")
 
     def talk(self):
         print(f"Im your enemy {self.type_of_enemy}. I'm here to destroy you")
 
     def start_attack(self):
-        print(f"Im attacking you with attack of {self.attack} point")
+        print(f"Im attacking you with attack of {self.attack_points} point")
 
     def walk(self):
         print(f"Enemy {self.type_of_enemy} is walking around")
--- day3/main.py
+++ day3/main.py
@@ -1,11 +1,8 @@
 from Enemy import *
 
-enemy = Enemy()
-enemy.type_of_enemy = 'Zombie'
-enemy.health_points = 100
-enemy.attack = 1
+zombie = Enemy('Mirurthan')
 
-enemy.about_enemy()
-enemy.talk()
-enemy.walk()
-enemy.start_attack()
+zombie.about_enemy()
+zombie.talk()
+zombie.walk()
+zombie.start_attack()
